baadbed7f3cd5657a9de0c540286cba44076ebab
Joao Victor Almeida <aalmeidadev@gmail.com>
2020-02-15 18:20:04 -0200

    ...

--- app/services/faq/translate_service.rb
+++ app/services/faq/translate_service.rb
@@ -9,7 +9,8 @@ module FaqModule
   
     def call
 
-    response = HTTParty.get("https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20200214T231324Z.6944657eeef2c0df.aabfba6a610902f657a9abc4e238d9b7b3240594&text=#{@phrase}&lang=#{@in_language}-#{@for_language}&format=plain")
+    response = HTTParty.get("https://translate.yandex.net/api/v1.5/tr.json/translate?key=#{ENV["SECRET_KEY"]}&text=#{@phrase}&lang=#{@in_language}-#{@for_language}&format=plain")
+    
     response_json["text"].first
       
     end
