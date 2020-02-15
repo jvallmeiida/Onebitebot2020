bd0674b0874b2e421c849284789cbb4a48df0b3e
Joao Victor Almeida <aalmeidadev@gmail.com>
2020-02-15 12:30:13 -0200

    corrigindo erros de syntax

--- app/services/faq/translate_service.rb
+++ app/services/faq/translate_service.rb
@@ -2,7 +2,7 @@ module FaqModule
   class TranslateService
     def initialize(params)
       @in_language = params["in_language"]
-      @for_language = params["in_language"]
+      @for_language = params["for_language"]
       @phrase = params["phrase"]
     end
   
