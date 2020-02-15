5e0c82be923a1e5eae6e599139330284dc5a8dcf
Joao Victor Almeida <aalmeidadev@gmail.com>
2020-02-15 11:31:30 -0200

    adicionando uma linha para ver se o texto está sendo traduzido

--- app/services/faq/translate_service.rb
+++ app/services/faq/translate_service.rb
@@ -11,6 +11,7 @@ module FaqModule
 
     response = HTTParty.get("https://translate.yandex.net/api/v1.5/tr.json/translate?key=#{ENV["SECRET_KEY"]}&text=#{@phrase}&lang=#{@in_language}-#{@for_language}&format=plain")
     response
+    puts "------------- PARA AQUI -----------"
       
       
     end
