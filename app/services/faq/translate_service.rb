1383700a368942202f725b5f5204f44945228d6c
Joao Victor Almeida <aalmeidadev@gmail.com>
2020-02-15 12:50:10 -0200

    tirando o puts do response

--- app/services/faq/translate_service.rb
+++ app/services/faq/translate_service.rb
@@ -10,7 +10,7 @@ module FaqModule
     def call
 
     response = HTTParty.get("https://translate.yandex.net/api/v1.5/tr.json/translate?key=#{ENV["SECRET_KEY"]}&text=#{@phrase}&lang=#{@in_language}-#{@for_language}&format=plain")
-    puts response
+    response
       
       
     end
