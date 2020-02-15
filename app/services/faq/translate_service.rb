54aa8a415f325e8d918d2c518d678b55e6cd0ae3
Joao Victor Almeida <aalmeidadev@gmail.com>
2020-02-15 18:17:35 -0200

    adicionando um .first em responseJson

--- app/services/faq/translate_service.rb
+++ app/services/faq/translate_service.rb
@@ -10,7 +10,7 @@ module FaqModule
     def call
 
     response = HTTParty.get("https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20200214T231324Z.6944657eeef2c0df.aabfba6a610902f657a9abc4e238d9b7b3240594&text=#{@phrase}&lang=#{@in_language}-#{@for_language}&format=plain")
-    response_json["text"]
+    response_json["text"].first
       
     end
   end
