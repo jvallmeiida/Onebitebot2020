59656a965b7f85b54074d1fb2dbe1dd52e2565ce
Joao Victor Almeida <aalmeidadev@gmail.com>
2020-02-16 20:27:33 -0300

    ajustando minhas quebras de lingua e adicionando uma frase na tradução do translate

--- app/services/faq/translate_service.rb
+++ app/services/faq/translate_service.rb
@@ -5,14 +5,9 @@ module FaqModule
       @for_language = params["for_language"]
       @phrase = params["phrase"]
     end
-  
-  
     def call
-
     response = HTTParty.get("https://translate.yandex.net/api/v1.5/tr.json/translate?key=#{ENV["SECRET_KEY"]}&text=#{@phrase}&lang=#{@in_language}-#{@for_language}&format=plain")
-    response["text"].first
-
-      
+    response "Essa é a frase que você deseja traduzir"+["text"].first
     end
   end
 end
