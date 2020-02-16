fa8ca9e0378f282a2e2b8a6b8e9e9e34f9fbbf5f
Joao Victor Almeida <aalmeidadev@gmail.com>
2020-02-16 20:35:50 -0300

    interpolando frases a resposta  do translate

--- app/services/faq/translate_service.rb
+++ app/services/faq/translate_service.rb
@@ -7,7 +7,7 @@ module FaqModule
     end
     def call
     response = HTTParty.get("https://translate.yandex.net/api/v1.5/tr.json/translate?key=#{ENV["SECRET_KEY"]}&text=#{@phrase}&lang=#{@in_language}-#{@for_language}&format=plain")
-    response "Essa é a frase que você deseja traduzir"+["text"].first
+    "essa é a tradução da sua frase #{response ['text'].first}"
     end
   end
 end
